9c0df8efeeb8d608bc1080087256f9a40db57f5f
Dan Payne <dpayne5532@gmail.com>
2022-01-29 21:33:59 -0700

    Initial Commit

---
+++ PDF417Scanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PDF417Scanner
+//
+//  Created by Dan Payne on 1/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PDF417Scanner/PDF417ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDF417ScannerApp.swift
+//  PDF417Scanner
+//
+//  Created by Dan Payne on 1/29/22.
+//
+
+import SwiftUI
+
+@main
+struct PDF417ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
